0ba25f3e542096ab1ae77175cd89034db43a1d4d
simeee <whdk2340@gmail.com>
2023-07-19 13:47:20 +0900

    특정문자열 출력

---
+++ day1/data_types.py
@@ -0,0 +1,12 @@
+quote ="Life is too short"
+print(quote.replace("too","very"))
+print(quote.replace("o","0"))
+print(quote.split(" "))
+
+quote2 = "learn Python"
+print(quote2.split()) #{"Learn","Python"}
+print(quote2.split("P")) #{"Learn ","ython"}
+print(quote2.split("arn")) #{"Le"," Python"}
+
+example ="a:B:C:D"
+print()
---
+++ day1/exercise.py
@@ -0,0 +1,10 @@
+a ="Dog is Love"
+print(a[7:])
+print(a.replace("Dog","Cat"))
+
+b="lalalalalalalalalalala"
+print(b.count("a"))
+print(len(b))
+
+c="010-1234-5678"
+print(c.replace("-",""))
