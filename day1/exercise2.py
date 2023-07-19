572ce24c32aba1abce863100f5575cd8c1e62442
simeee <whdk2340@gmail.com>
2023-07-19 14:56:25 +0900

    list 데이터타입 실습

---
+++ day1/exercise2.py
@@ -0,0 +1,6 @@
+a = ['red', 'pink', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'white']
+a.remove("green")
+print(a)
+
+a = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'white']
+a.insert(1,"pink")
