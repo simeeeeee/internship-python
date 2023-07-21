1e2393605e153d131df1fd24dc8368bedcb21229
simeee <whdk2340@gmail.com>
2023-07-21 18:03:29 +0900

    try/except 구현

--- day2/Django/products/views.py
+++ day2/Django/products/views.py
@@ -27,17 +27,28 @@ def book_list(request):
     return response
 
 def book_detail(request, pk):
-    book = Book.objects.get(pk = pk)  #특정 Book object를 가져온다
-    data = {
-        "book" : {
-            "author": book.author.name,
-            "name": book.name,
-            "description": book.description,
-            "shipping_cost": book.shipping_cost,
-            "quantity": book.quantity
+    try:
+        book = Book.objects.get(pk = pk)  #특정 Book object를 가져온다
+        data = {
+            "book" : {
+                "author": book.author.name,
+                "name": book.name,
+                "description": book.description,
+                "shipping_cost": book.shipping_cost,
+                "quantity": book.quantity
+            }
         }
-    }
-    response = JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
-    return response
-
+        response = JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+        return response
+    except Book.DoesNotExist:
+        print("Book object doesn't exist")
+        return None
     ## 원래는 없는 경우에 대비해서 try / except Book.DoesnotExist  등 처리를 해줘야 함
+    
+    """
+    과제 (24일 오후 11시)
+    1. 구현하지 못한 try/except 를 구현하거나
+    2. model 에 좀 더 추가해서 조금 더 다채롭게 만들어보거나
+    3. ppt를 보고 ChatGPT API를 사용해서 뭔가를 만들어보거나
+    4. Codespace 로 배포해보기
+    """
