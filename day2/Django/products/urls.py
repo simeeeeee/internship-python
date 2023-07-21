416871e7e87a5fbf931ffbdc2f59092adbfa45d0
simeee <whdk2340@gmail.com>
2023-07-21 17:52:34 +0900

    json형태로 mvt

--- day2/Django/products/urls.py
+++ day2/Django/products/urls.py
@@ -4,11 +4,12 @@
 from django.urls import path
 
 #from .views import BookDetailView, BookListView
-from .views import book_list
+from .views import book_list, book_detail
 
 
 urlpatterns = [
-   # path("books", BookListView.as_view(), name="book-list"),
+   # path("books", BookListView.as_view(), name="book-list"),  #.as_view() -> html형태로 보냄
    # path("books/<int:pk>", BookDetailView.as_view(), name="book-detail")
-   path("books", book_list, name="book_list"),
+   path("books", book_list, name="book_list"),   #json으로 받을거야
+   path("books/<int:pk>", book_detail, name="book-detail")
 ]
--- day2/Django/products/views.py
+++ day2/Django/products/views.py
@@ -25,3 +25,19 @@ def book_list(request):
     #return response
     response = JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
     return response
+
+def book_detail(request, pk):
+    book = Book.objects.get(pk = pk)  #특정 Book object를 가져온다
+    data = {
+        "book" : {
+            "author": book.author.name,
+            "name": book.name,
+            "description": book.description,
+            "shipping_cost": book.shipping_cost,
+            "quantity": book.quantity
+        }
+    }
+    response = JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+    return response
+
+    ## 원래는 없는 경우에 대비해서 try / except Book.DoesnotExist  등 처리를 해줘야 함
